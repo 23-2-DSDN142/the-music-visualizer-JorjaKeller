709ffae62cd735aa885dcd27cc04ca76724fc1ca
JorjaKeller <jorja.keller@gmail.com>
2023-10-20 04:03:54 +1300

    final commit
    final commit

--- music_starter.js
+++ music_starter.js
@@ -1,50 +1,66 @@
 
+let beanMode =false;
+let dinos;
+let dinos2;
+let firstRun=true;
+let meteor;
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
+  background(59, 12, 12);
   textFont('Helvetica'); // please use CSS safe fonts
-  rectMode(CENTER)
+  rectMode(CENTER);
   textSize(24);
-
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
-   let red = color(201, 36, 39)
-   let purple = color(178, 40, 199)
   
-   let LerpMap = map(bass,0,100,0,1)
-   let middleColor = lerpColor(red,purple,LerpMap)
-  
-   fill(middleColor)
-   ellipse(100,100,100,100)
+  if(firstRun){
+    meteor = loadImage('meteor.png');
+    dinos = loadImage('Dinos.0.png');
+    dinos2  = loadImage('Dinos.1.png');
+    firstRun = false;
+  }
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
+
+  drumMap = map(drum,60,70,0.5,0.6);
+  bassMap = map(bass,0,100,1,2);
+
  
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
+  
+  
+push();
+translate(200,-125);
+  scale(drumMap);
+  image(meteor,70,0);
+  pop();
+
+
+
+if(vocal > 50){beanMode = true;}
+
  
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   //text(words, width/2, height/3);
+  vocalMap = map(vocal, 0,80,50,200);
+  let lineStart = -5;
+  let lengthOfLine = 300;
+  let lineEnd = lineStart + lengthOfLine;
+  stroke(245, 203, 135);
+  strokeWeight(vocalMap);
+
+  if(beanMode){;
+  
+    for(let i =1; i <=vocalMap; i++){;
+    line(100,lineStart,100,lineEnd);
+  }};
+
+  noStroke();
+  fill(89, 23, 23);
+  rect(270,435,590,300);
+
+  if(bass>50){image(dinos,0,0)}
+  else{(image(dinos2,0,0))};
+
+
+  fill(79, 11, 20);
+  noStroke();
+  rect(100,250,250,100);
+  triangle(225,200,300,300,225,300);
+
+
 }
